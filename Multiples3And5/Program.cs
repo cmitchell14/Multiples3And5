7a4eff8fe91af56087ca8b677dd35ff66a8bfb69
cmitchell14 <calmitch14@gmail.com>
2021-10-04 15:40:35 -0500

    Initial Commit

--- Multiples3And5/Program.cs
+++ Multiples3And5/Program.cs
@@ -15,11 +15,7 @@ namespace Multiples3And5
 
             //Create a new, public repository and add a blank solution.Add a new Console Application project called Multiples3And5.
 
-            //If we list all the natural numbers below 10
-            //that are multiples of 3 or 5, we get 3, 5,
-            //6 and 9. The sum of these multiples is 23.
-
-            //Find the sum of all the multiples of 3 or 5 below 1000 and print it out to the console.
+            //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.  Find the sum of all the multiples of 3 or 5 below 1000 and print it out to the console.
 
         }
     }
