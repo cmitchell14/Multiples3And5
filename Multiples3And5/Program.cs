55e4c960141e41d7bd7894e0444061c799306c90
cmitchell14 <calmitch14@gmail.com>
2021-10-04 16:17:56 -0500

    Completed Mutlitples of 3 and 5 challenge.

--- Multiples3And5/Program.cs
+++ Multiples3And5/Program.cs
@@ -17,6 +17,34 @@ namespace Multiples3And5
 
             //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.  Find the sum of all the multiples of 3 or 5 below 1000 and print it out to the console.
 
+            Console.WriteLine("If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.  Find the sum of all the multiples of 3 or 5 below 1000 and print it out to the console.\n\nPress any button to see the answer:");
+            Console.ReadKey();
+
+            int userNbr = 1000;
+            List<int> Mutliples3And5 = new List<int>();
+
+
+            Console.Title = "ARTP - Coding Challenge 1";
+
+            for (int i = 1; i < userNbr; i++)
+            {
+
+                if (i % 3 == 0)
+                {
+                    Mutliples3And5.Add(i);
+                }
+                else if (i % 5 == 0)
+                {
+                    Mutliples3And5.Add(i);
+                }
+                else if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Mutliples3And5.Remove(i);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(Mutliples3And5.Sum());
+
         }
     }
 }
---
+++ Multiples3And5Library/MultiplesOf3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiples3And5Library
+{
+    public class MultiplesOf3
+    {
+        //Props
+        public int MutlipleOf3 { get; set; }
+
+        //ctor
+
+        //Method
+        public void MultipleOfThree(int MutipleOf3)
+        {
+            if (MutlipleOf3 = % 3)
+            {
+
+            }
+        }
+    }
+}
